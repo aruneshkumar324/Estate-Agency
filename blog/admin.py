bfb33587f47dd86e0f5bb03ad597ba0aa85cfd29
Arunesh kumar <aruneshkumar324@gmail.com>
2022-03-13 19:26:29 +0530

    2.2 : ReInstall Blog App

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from .models import Blog
 
-
-class BlogAdmin(admin.ModelAdmin):
-    readonly_fields = ['created_date', 'updated_date',]
-
-admin.site.register(Blog, BlogAdmin)
+# Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,3 @@
 from django.db import models
-from ckeditor.fields import RichTextField
 
-
-class Blog(models.Model):
-    title = models.CharField(max_length=250)
-    category = models.CharField(max_length=200)
-    author = models.CharField(max_length=250)
-    photo = models.ImageField(upload_to='blog/%Y/%m/%d/')
-    description = RichTextField()
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    # created_date = models.DateTimeField(default=datetime.now, blank=True)
-
-    def __str__(self):
-        return self.title
+# Create your models here.
